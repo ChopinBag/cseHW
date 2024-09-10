4dd8902404d3ecb0a3920abac1fcfc724a50c9ea
ChopinBag <sw6129@pusan.ac.kr>
2024-09-10 11:59:45 +0900

    Remove CSS, HTML, and JavaScript files for final exam

--- Practice/Intro_to_Internet&Web/Final exam/note.js
+++
@@ -1,7 +0,0 @@
-function alertMouse() {
-    let d1 = document.getElementById('div1');
-    let text = document.createElement("DIV");
-    text.innerHTML = '마우스를 올려?';
-    d1.appendChild(text)
-}
-
