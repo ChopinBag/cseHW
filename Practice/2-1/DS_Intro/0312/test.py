16c814bd288fccbb4f3d77314a0938eed74b35d8
chopinbag <sw6129@pusan.ac.kr>
2025-03-18 16:09:36 +0900

    0318

--- Practice/2-1/DS_Intro/0312/test.py
+++ Practice/2-1/DS_Intro/0312/test.py
@@ -1,2 +1,9 @@
 import pandas as pd
 import matplotlib as plt
+
+df = pd.read_csv('auto.csv')
+df._get_numeric_data()
+numeric = df.select_dtypes(include='number')
+categorical = df.select_dtypes(exclude='number')
+
+
--- Practice/2-1/cpp_study/test.cpp
+++ Practice/2-1/cpp_study/test.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
+using namespace std;
+
+int main(){
+    int num = 0;
+    cin >> num;
+    int *arr = new int[num][99];
+
+    for (int i=0; i < num; i++){
+        cin >> arr[i];
+    }
+
+    int input_i = 0;
+    cin >> input_i;
+
+    cout << arr[arr[input_i]] << endl;
+
+    delete arr;
     return 0;
 }
