6cad9d249c9d32b8b35691ca5233403b3c568f61
chopinbag <sw6129@pusan.ac.kr>
2025-04-06 19:52:56 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Practice/2-1/cpp_study/cmake-build-debug/CMakeFiles/clion-Debug-log.txt

---
+++ Practice/2-1/cpp_study/cmake-build-debug/CMakeFiles/datalab.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/imseung-u/CLionProjects/cseHW/Practice/2-1/System_Software/test_for_datalab.c" "CMakeFiles/datalab.dir/Users/imseung-u/CLionProjects/cseHW/Practice/2-1/System_Software/test_for_datalab.c.o" "gcc" "CMakeFiles/datalab.dir/Users/imseung-u/CLionProjects/cseHW/Practice/2-1/System_Software/test_for_datalab.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Practice/2-1/cpp_study/cmake-build-debug/CMakeFiles/tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests.dir/Users/imseung-u/CLionProjects/cseHW/Practice/test_C.c.o"
+  "CMakeFiles/tests.dir/Users/imseung-u/CLionProjects/cseHW/Practice/test_C.c.o.d"
+  "tests"
+  "tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
