ca7c249b90a61c57ebf08f0f3268659b47566d9e
chopinbag <sw6129@pusan.ac.kr>
2025-05-25 14:40:22 +0900

    done : PR095

--- Practice/2-1/cpp/Pr/Pr095/Employee.h
+++ Practice/2-1/cpp/Pr/Pr095/Employee.h
@@ -14,6 +14,7 @@ public:
         cout << "Salary: " << salary_ << endl;
         cout << "Years of service: " << years_of_service_ << endl;
         cout << "Performance score: " << performance_score_ << endl;
+        cout << trait_ << endl;
     }
     string get_name() const {
         return name_;
@@ -23,30 +24,32 @@ protected:
     int salary_;
     int years_of_service_ = 0;
     int performance_score_ = 0;
+    string trait_;
 };
 
 class Manager : public Employee{
     public:
-        string name_;
-        int salary_;
         string department_;
         Manager(string name, int salary, string department)
-        : name_{name}, salary_{salary}, department_{department}
-        {}
+        : Employee(name, salary), department_{department}
+        {
+            trait_ = "Department: " + department_;
+        }
         void set_goals(){cout << "Set goals for " << department_ << endl;}
+        
     private:
         
 };
 
 class Programmer : public Employee{
     public:
-        string name_;
-        int salary_;
         string lang_;
         Programmer()=default;
         Programmer(string name, int salary, string lang)
-        : name_{name}, salary_{salary}, lang_{lang}
-        {};
+        : Employee(name, salary), lang_{lang}
+        {
+            trait_ = "Language: " + lang_; 
+        };
         void write_code(){
             cout << "Writing code in " << lang_ << endl;
             performance_score_ += 10;
@@ -57,12 +60,12 @@ class Programmer : public Employee{
 
 class Salesperson : public Employee{
     public:
-        string name_;
-        int salary_;
         int revenue_;
         Salesperson(string name, int salary, int revenue)
-        : name_{name}, salary_{salary}, revenue_{revenue}
-        {};
+        : Employee(name, salary), revenue_{revenue}
+        {
+            trait_ = "Sales: " + to_string(revenue_);
+        };
         void sell(){
             cout << "Selling products, generated " << revenue_ << " in revenue" << endl;
             performance_score_ += 20;
