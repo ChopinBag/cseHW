05a3fae5f0de68fa9d7c087a035ee65496cf7885
chopinbag <sw6129@pusan.ac.kr>
2025-06-04 18:06:15 +0900

    Done : Pr102

--- Lecture/2-1/cpp/Pr/Pr102/my_vector.hpp
+++ Lecture/2-1/cpp/Pr/Pr102/my_vector.hpp
@@ -1,24 +1,39 @@
 #ifndef MYVECTOR_HPP
 #define MYVECTOR_HPP
 #include <initializer_list>
+#include <algorithm>
 
 using namespace std;
 
 template<typename T>
 class my_vector{
     public:
-        my_vector(T t=0)
-            :data_{t? new T[t] : nullptr}, size_{t}{}
+        explicit my_vector(size_t n=0) noexcept
+            :data_{n? new T[n] : nullptr}, size_{n}{}
 
-        my_vector(initializer_list<T> lst)
-            :data_{lst.size() ? new T[lst.size()] : nullptr}, size_{lst.size()}{}
+        my_vector(initializer_list<T> lst) noexcept
+            :data_{lst.size() ? new T[lst.size()] : nullptr}, size_{lst.size()}{
+                copy(lst.begin(), lst.end(), data_);
+                size_ = lst.size();
+            }
+
+        my_vector(const my_vector<T>& other) noexcept
+        : data_(other.size_ ? new T[other.size_] : nullptr), size_(other.size_)
+        {
+            for (size_t i = 0; i < size_; ++i)
+                data_[i] = other.data_[i];
+        }
         
         T& operator[](T i){
             return data_[i];
         }
 
-        int size(){
+        size_t size(){
             return size_;
+        }
+
+        ~my_vector(){
+            delete[] data_;
         }
 
     private:
